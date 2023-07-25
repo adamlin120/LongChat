926c88cf7c6c893362cfe2a312b8812f445e8890
Yen-Ting Lin <ytl@ieee.org>
2023-07-25 02:58:03 -0700

    🎉 Added stage3_no_offloading.conf configuration file 🔧 Updated pretrain_sft_flash.sh script to use deepspeed with stage3_no_offloading.conf 🔄 Updated sft_trainer.py to include deepspeed training option

--- pretrain_sft_flash.sh
+++ pretrain_sft_flash.sh
@@ -18,26 +18,6 @@ export WANDB_TAGS="${MODEL_NAME},zh_c4,zh_wiki,en_wiki,${MODEL_SIZE},fsdp,trl"
 
 echo "Training llama2 model: ${MODEL_NAME}"
 echo "using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-
-# first cli argument is the model size, default is 7
-# second cli argument is the batch size per gpu, default is 1
-MODEL_SIZE_ARG=${1:-7}
-BATCH_SIZE_PER_GPU=${2:-1}
-DEBUG=${3:-0}
-
-MODEL_SIZE="${MODEL_SIZE_ARG}b"
-NUM_GPUS=8
-TOTAL_BATCH_SIZE=8
-GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
-
-MODEL_NAME="meta-llama/Llama-2-${MODEL_SIZE}-hf"
-
-export WANDB_PROJECT="Chinese-LLAMA2"
-export WANDB_TAGS="${MODEL_NAME},zh_c4,zh_wiki,en_wiki,${MODEL_SIZE},fsdp,trl"
-
-echo "Training llama2 model: ${MODEL_NAME}"
-echo "using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
 
 
 python -m torch.distributed.run \
